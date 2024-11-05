51abd0c27e327e551b2024cd913c42b68b1c1ee4
Andrea <agarreta2@gmail.com>
2024-11-05 16:24:31 +0100

    Stat login

---
+++ Vista/InicioSesion/login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/static/style.css">
+</head>
+<body>
+    <div class="login-container">
+        <h2>Login - Hotel Chain</h2>
+        <form>
+            <input type="text" placeholder="Username" required>
+            <input type="password" placeholder="Password" required>
+            <button type="submit">Login</button>
+            <a href="#" class="register-link">Create an account</a>
+        </form>
+    </div>
+</body>
+</html>
