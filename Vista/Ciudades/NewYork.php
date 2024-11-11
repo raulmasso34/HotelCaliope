38d1ca859c008f3613d0f3aebe35cd2089182d44
Andrea <agarreta2@gmail.com>
2024-11-11 20:18:33 +0100

    añadir index.css

--- Vista/Ciudades/NewYork.php
+++ Vista/Ciudades/NewYork.php
@@ -3,52 +3,53 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../static/styles/cities.css">
     <title>Document</title>
 </head>
 <body>
     <header>
 
-<section id="logo" class="logo">
-  <img src="../../static/img/logo.png" alt="logo">
-</section>
+        <section id="logo" class="logo">
+        <img src="../../static/img/logo.png" alt="logo">
+        </section>
 
-<section id="menu" class="menu">
-  <table>
-  <td>
-  <div class="dropdown">
-  <a href="../Ciudades/NewYork.php">NewYork</a>
-  <div class="dropdown-content">
-  <a href="#">Blog</a>
-  <a href="#">Academy</a>
-  <a href="#">YouTube</a>
-  </div>
-</div>
-</td>
-  <td>
-  <div class="dropdown">
-  <a href="#">HubSpot Resources</a>
-  <div class="dropdown-content">
-  <a href="#">Blog</a>
-  <a href="#">Academy</a>
-  <a href="#">YouTube</a>
-  </div>
-</div>
-</td>
-  <td>
-  <div class="dropdown">
-  <a href="#">HubSpot Resources</a>
-  <div class="dropdown-content">
-  <a href="#">Blog</a>
-  <a href="#">Academy</a>
-  <a href="#">YouTube</a>
-  </div>
-</div>
-</td>
-</table>
-</section>
-<section id="iniciar" class="iniciar">
-<a href="../InicioSesion/login.php"><img src="../../imagenes/header/iniciar.png" alt="iniciar"></a>
-</section>
+        <section id="menu" class="menu">
+        <table>
+        <td>
+        <div class="dropdown">
+        <a href="../Ciudades/NewYork.php">NewYork</a>
+        <div class="dropdown-content">
+        <a href="#">Blog</a>
+        <a href="#">Academy</a>
+        <a href="#">YouTube</a>
+        </div>
+        </div>
+        </td>
+        <td>
+        <div class="dropdown">
+        <a href="#">HubSpot Resources</a>
+        <div class="dropdown-content">
+        <a href="#">Blog</a>
+        <a href="#">Academy</a>
+        <a href="#">YouTube</a>
+        </div>
+        </div>
+        </td>
+        <td>
+        <div class="dropdown">
+        <a href="#">HubSpot Resources</a>
+        <div class="dropdown-content">
+        <a href="#">Blog</a>
+        <a href="#">Academy</a>
+        <a href="#">YouTube</a>
+        </div>
+        </div>
+        </td>
+        </table>
+        </section>
+        <section id="iniciar" class="iniciar">
+        <a href="../InicioSesion/login.php"><img src="../../imagenes/header/iniciar.png" alt="iniciar"></a>
+        </section>
     </header>
 
 
