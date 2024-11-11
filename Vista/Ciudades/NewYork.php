2bfd5581c8dec5784d1222a5b88e4bdc9e7c6b1f
Andrea <agarreta2@gmail.com>
2024-11-11 20:13:39 +0100

    cambios 008 11/11

--- Vista/Ciudades/NewYork.php
+++ Vista/Ciudades/NewYork.php
@@ -6,6 +6,56 @@
     <title>Document</title>
 </head>
 <body>
-    Hola
+    <header>
+
+<section id="logo" class="logo">
+  <img src="../../static/img/logo.png" alt="logo">
+</section>
+
+<section id="menu" class="menu">
+  <table>
+  <td>
+  <div class="dropdown">
+  <a href="../Ciudades/NewYork.php">NewYork</a>
+  <div class="dropdown-content">
+  <a href="#">Blog</a>
+  <a href="#">Academy</a>
+  <a href="#">YouTube</a>
+  </div>
+</div>
+</td>
+  <td>
+  <div class="dropdown">
+  <a href="#">HubSpot Resources</a>
+  <div class="dropdown-content">
+  <a href="#">Blog</a>
+  <a href="#">Academy</a>
+  <a href="#">YouTube</a>
+  </div>
+</div>
+</td>
+  <td>
+  <div class="dropdown">
+  <a href="#">HubSpot Resources</a>
+  <div class="dropdown-content">
+  <a href="#">Blog</a>
+  <a href="#">Academy</a>
+  <a href="#">YouTube</a>
+  </div>
+</div>
+</td>
+</table>
+</section>
+<section id="iniciar" class="iniciar">
+<a href="../InicioSesion/login.php"><img src="../../imagenes/header/iniciar.png" alt="iniciar"></a>
+</section>
+    </header>
+
+
+
+
+    <footer>
+
+    </footer>
 </body>
 </html>
