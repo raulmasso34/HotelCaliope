f574c2bc3773735a461fccf24ed5be721a6d5393
Andrea <agarreta2@gmail.com>
2024-12-19 16:47:16 +0100

    cambios galeria

--- vista/Contacto/contacto.php
+++ vista/Contacto/contacto.php
@@ -14,11 +14,10 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;1,600&family=Luxurious+Roman&family=Mate+SC&family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&family=Old+Standard+TT:ital,wght@0,400;0,700;1,400&family=Oswald:wght@200..700&family=Patrick+Hand&family=Permanent+Marker&family=Rancho&family=Shadows+Into+Light&family=Staatliches&display=swap" rel="stylesheet">
 
-    <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;1,600&family=Luxurious+Roman&family=Mate+SC&family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&family=Oswald:wght@200..700&family=Patrick+Hand&family=Permanent+Marker&family=Rancho&family=Shadows+Into+Light&family=Staatliches&display=swap" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../static/css/style.css">
+    <link rel="stylesheet" href="../../static/css/style.css">
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
      integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
      crossorigin=""/>
@@ -31,25 +30,25 @@
 <body>
     <header class="main-header">
     <div class="carousel">
-        <img class="carousel-background" src="../static/img/florida/florida3.jpg" alt="Fondo 1">
-        <img class="carousel-background" src="../static/img/florida/florida4.jpg" alt="Fondo 2">
-        <img class="carousel-background" src="../static/img/florida/florida5.jpg" alt="Fondo 3">
+        <img class="carousel-background" src="../../static/img/florida/florida3.jpg" alt="Fondo 1">
+        <img class="carousel-background" src="../../static/img/florida/florida4.jpg" alt="Fondo 2">
+        <img class="carousel-background" src="../../static/img/florida/florida5.jpg" alt="Fondo 3">
     </div>
        
         <section class="main-up">
             <div class="main-up-left">
-                <img src="../static/img/logo.png" alt="Imagen secundaria">
+               <a href="../index.php"><img src="../../static/img/logo.png" alt="Imagen secundaria"></a> 
             </div>
             <div class="main-up-right">
                 <div class="links">
-                    <a href="../vista/Habitaciones/habitaciones.php">Habitaciones</a>
+                    <a href="../../vista/Habitaciones/habitaciones.php">Habitaciones</a>
                     
                     <div class="dropdown">
                         <a href="#" class="dropbtn">Hoteles</a>
                         <div class="dropdown-content">
                             <div class="dropdown-section">
                                 <h4>Europa</h4>
-                                <a href="../vista/ciudades/Europa/Galicia.php">Galicia</a>
+                                <a href="../../vista/ciudades/Europa/Galicia.php">Galicia</a>
                                 <a href="#">Tossa de Mar</a>
                                 <a href="#">Pirineos</a>
                             </div>
@@ -61,8 +60,9 @@
                             </div>
                         </div>
                     </div>
-                    <a href="../vista/galeria/galeria.php">Galerias</a>
-                <a href="../vista/ofertas/ofertas.php">Ofertas</a>
+                    <a href="../../vista/galeria/galeria.php">Galeria</a>
+                <a href="../../vista/ofertas/ofertas.php">Ofertas</a>
+                <a href="../../vista/Contacto/contacto.php">Contacto</a>
             </div>
         
 
@@ -103,5 +103,12 @@
           
         </section>
     </header>
+
+
+    <div class="main">
+
+    </div>
+
+
 </body>
 </html>
