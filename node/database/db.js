3b16a7802d083fe251685356791a72df5f6012ea
Andrea <agarreta2@gmail.com>
2025-01-30 16:37:09 +0100

    no se

--- node/database/db.js
+++ node/database/db.js
@@ -3,16 +3,16 @@ const mysql = require('mysql');
 const conexion = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'password',
+    password: 'P@ssw0rd',
     database: 'HotelCaliope'
 })
 
-conexion.connect((error)=>{
-    if(error){
-        console.error('El error de conexión es '+error);
+conexion.connect((error) => {
+    if (error) {
+        console.error('El error de conexión es: ' + error.message); // Mejor manejo de errores
+        return;
     }
-
-    console.log('conexion realizada')
-})
+    console.log('Conexión realizada');
+});
 
 module.exports = conexion;
